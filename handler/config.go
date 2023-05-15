09904ae588e0c3846b6595acd781f8272e8efcbd
Igor S. Morozov <igor.morozov@republic.co>
2023-05-15 19:29:06 +0300

    Run gci

--- handler/config.go
+++ handler/config.go
@@ -10,8 +10,8 @@ type nonceGenerator func() []byte
 
 // Config holds the plugin configuration.
 type Config struct {
-	LastModified   bool   `json:"lastModified" toml:"lastModified" yaml:"lastModified"`
-	Placeholder    string `json:"placeholder" toml:"placeholder" yaml:"placeholder" default:"DhcnhD3khTMePgXw"`
+	LastModified   bool                      `json:"lastModified" toml:"lastModified" yaml:"lastModified"`
+	Placeholder    string                    `json:"placeholder" toml:"placeholder" yaml:"placeholder" default:"DhcnhD3khTMePgXw"`
 	NonceGenerator nonceGenerator            `json:"-" toml:"-" yaml:"-"`
 	LogLevel       int8                      `json:"logLevel" toml:"logLevel" yaml:"logLevel"`
 	Monitoring     httputil.MonitoringConfig `json:"monitoring" toml:"monitoring" yaml:"monitoring"`
